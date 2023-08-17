4c293631010c99368d5a3cff42ed41b49a1a4a63
Rinkal <email@example.com>
2023-08-17 15:29:08 +0100

    add for loop to display five days forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,67 @@ $(document).ready(function() {
     </div>
         `;
         $("#today").html(currentWeatherHTML);
+
+        
+var forecastHTML = '<h2>5-Day Forecast</h2><div class="row">';
+var forecastsPerDay = {}; // Group forecasts by day
+// Calculate the date for tomorrow
+var tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate());
+
+// Initialize a counter for the number of days included
+var daysIncluded = 0;
+
+
+for (var i = 0; i < forecastData.list.length; i++) {
+    var forecast = forecastData.list[i];
+    var forecastDate = new Date(forecast.dt_txt.split(' ')[0]); // Convert to Date object
+
+    // Check if the forecast date is from tomorrow to the next 5 days
+    if (forecastDate >= tomorrow && daysIncluded < 5) {
+        var formattedDate = forecastDate.toLocaleDateString('en-GB'); // Format as dd/mm/yyyy
+        if (!forecastsPerDay[formattedDate]) {
+            forecastsPerDay[formattedDate] = {
+                date: formattedDate,
+                icon: forecast.weather[0].icon,
+                temperatures: [],
+                winds: [],
+                humidities: []
+            };
+
+            daysIncluded++; // Increment the counter
+        }
+
+        forecastsPerDay[formattedDate].temperatures.push(forecast.main.temp);
+        forecastsPerDay[formattedDate].winds.push(forecast.wind.speed);
+        forecastsPerDay[formattedDate].humidities.push(forecast.main.humidity);
+    }
+}
+
+// Create a card for each day's forecast
+Object.values(forecastsPerDay).forEach(dayForecast => {
+    forecastHTML += `
+        <div class="col-lg">
+          <div class="card">
+            <div class="card-body">
+              <h3 class="card-title">${dayForecast.date}</h3>
+              <img src="http://openweathermap.org/img/w/${dayForecast.icon}.png" alt="Weather Icon">
+              <p class="card-text">Temp: ${average(dayForecast.temperatures)} °C</p>
+              <p class="card-text">Wind: ${average(dayForecast.winds)} MPH</p>
+              <p class="card-text">Humidity: ${average(dayForecast.humidities)}%</p>
+            </div>
+          </div>
+        </div>
+    `;
+});
+
+forecastHTML += '</div>';
+$("#forecast").html(forecastHTML);
+
+// Helper function to calculate average
+function average(arr) {
+    return (arr.reduce((sum, value) => sum + value, 0) / arr.length).toFixed(2);
+}
   
       }
 
